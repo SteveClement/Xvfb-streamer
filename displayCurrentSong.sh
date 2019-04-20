36b46b390bcbfbfd37bce34b2b8c0186e47eb625
Steve Clement <steve@localhost.lu>
2019-04-20 15:46:21 +0200

    new: [bash] added script to show current running song.

---
+++ displayCurrentSong.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+LOUNGE=http://64.71.79.181:5080/stream
+ffprobe -v quiet -print_format json -show_format -show_streams $LOUNGE |jq -r '.format | .tags| .StreamTitle' > /tmp/current.song
+
+if [[ "$(cat /tmp/current.song)" != "null" ]]; then
+  echo "Current song: $(cat /tmp/current.song)" | osd_cat -d 120 -A right -p bottom -f  -adobe-helvetica-bold-r-normal--34-240-100-100-p-176-iso8859-1 -c DodgerBlue -s 0
+fi
